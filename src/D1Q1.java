5c596a9965a1f051b0653175d824da0b0d605bf2
JJ Feeney <jjfeene3rd@gmail.com>
2022-12-05 18:40:59 -0500

    works

--- src/D1Q1.java
+++ src/D1Q1.java
@@ -5,27 +5,37 @@ import java.util.Scanner;
 public class D1Q1 {
         public static void main(String args[]) throws FileNotFoundException {
                 Scanner in = new Scanner(new File("Input/D1Q1Input.txt"));
-                int max = 0;
                 int maxNum = 0;
                 int cur = 0;
                 int number = 0;
                 int line = 0;
+                int first = 0;
+                int second = 0;
+                int third = 0;
                 while(in.hasNextLine()){
                         line++;
                         String temp = "0" + in.nextLine();
                         double tempNum = Integer.parseInt(temp);
                         if(tempNum == 0){
                                 number++;
-                                if(cur > max) {
-                                        max = cur;
-                                        maxNum = number;
+                                if(cur > third) {
+                                        if(cur > second){
+                                                if(cur > first){
+                                                        first = cur;
+                                                }else{
+                                                        second = cur;
+                                                }
+                                        }else{
+                                                third = cur;
+                                        }
+
                                 }
                                 cur = 0;
                         } else {
                                 cur += tempNum;
                         }
                 }
-                System.out.println(max);
+                System.out.println(third+second+first);
 
         }
 }
